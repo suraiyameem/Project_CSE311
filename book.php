05afa89834a2e72f1cfd830025a525ea42d6b29a
Suraiya Islam Meem <suraiya.meem@northsouth.edu>
2022-01-01 18:08:13 +0600

    new files added

--- book.php
+++ book.php
@@ -1,26 +1,29 @@
 <?php
 
-$link = mysqli_connect("localhost", "root", "", "hotel management");
-// Check connection
-if($link === false){
-die("ERROR: Could not connect. " . mysqli_connect_error());
+session_start();
+$con=mysqli_connect("localhost","root","");
+mysqli_select_db($con, "hotel management");
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$numofdays = $_POST["numofdays"];
+
+
+$sql = "SELECT * FROM book WHERE username='$username'";
+
+$result = mysqli_query($con, $sql);
+
+$n = mysqli_num_rows($result);
+
+if($n==1){
+	echo "Record is already added.";
+} 
+
+else 
+{
+    $reg = "INSERT INTO book (username, email, numofdays) VALUES ('$username','$email', '$numofdays')"; 
+    mysqli_query($con,$reg);
+    echo "Records added sucessfully";
 }
 
-// Escape user inputs for security
-$name = mysqli_real_escape_string($link, $_REQUEST['name']);
-$email = mysqli_real_escape_string($link, $_REQUEST['email']);
-$numofdays = mysqli_real_escape_string($link, $_REQUEST['numofdays']);
-$comment = mysqli_real_escape_string($link, $_REQUEST['comment']);
-
-// attempt insert query execution
-$sql = "INSERT INTO book (name, email, numofdays, comment) VALUES ('$name',
-'$email', '$numofdays','comment')";
-
-if(mysqli_query($link, $sql)){
-echo "Records added successfully.";
-} else{
-echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
-// close connection
-mysqli_close($link);
 ?>
